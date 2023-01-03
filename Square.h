f0bd127f6ed212fab153d0d95dabca21f1d047ae
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-03 23:52:43 +0900

    Create Square.h

---
+++ Square.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+
+
+
+class setSquare
+{
+	int avenue;
+	int height;
+public:
+	void Draw();
+	void Set(int _avenue, int _height);
+	void gotoxy(int x, int y, int Line);
+};
+
