42d2279d35314e803a687c5ed4f5df9c637e47c8
vhswo <cba0898@gmail.com>
2023-01-16 15:52:48 +0900

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -1,23 +1,42 @@
 #include "Map.h"
-
-
+#define WIDTH 20
+#define HEIGHT 20
+enum Select
+{
+	Start = 1,
+	Replay,
+	Setting,
+	Exit
+};
 
 Map::Map()
 {
-	cout << "맵 X 입력 :";
-	cin >> m_ix;
-	cout << "맵 Y 입력 :";
-	cin >> m_iy;
-	cout << "맵 가로 길이 입력 :";
-	cin >> m_iWidth;
-	cout << "맵 세로 길이 입력 :";
-	cin >> m_iHeight;
+	MapDraw::GameMapDraw(0, 0, WIDTH, HEIGHT);
 }
 
 
-void Map::MapDraw()
+void Map::UIDraw()
 {
-	MapDraw::BoxDraw(m_ix, m_iy, m_iWidth, m_iHeight);
+		int Select;
+		MapDraw::DrawMidText("1.게임 시작", WIDTH, 5);
+		MapDraw::DrawMidText("2.Replay", WIDTH, 7);
+		MapDraw::DrawMidText("3.게임 설정", WIDTH, 9);
+		MapDraw::DrawMidText("4.종료", WIDTH, 11);
+		MapDraw::BoxDraw(14, 13, 7, 3);
+		MapDraw::gotoxy(WIDTH, 14);
+		cin >> Select;
+		switch (Select)
+		{
+			case Start:
+				break;
+			case Replay:
+				break;
+			case Setting:
+				break;
+			case Exit:
+				return;
+		}
+
 }
 
 Map::~Map()
