50772db9b834c3226a49f656fb79a462572c61de
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-17 13:16:17 +0900

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -2,24 +2,14 @@
 #include"Mecro.h"
 #include"MapDraw.h"
 #include"conio.h"
+#include"GameSet.h"
+#include"PlayerInfo.h"
 
 class Game
 {
-	const string p1 = "A";
-	const string p2 = "B";
-	int p1_back = 5;//¹«¸£±â È½¼ö
-	int p2_back = 5;//¹«¸£±â È½¼ö
-	int turn = 1;
-	int start_y = 20;
-	int start_x = 20;
-	int* p1_stone = new int[100];
+	
 public:
-
-	void Mainplay();
+	void Mainplay(int playerType);
 	Game();
-	void 
-
-
-
-
+	
 };
