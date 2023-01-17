833a635253057d0cd7c2264c3badf262b7625292
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-17 13:16:24 +0900

    Create PlayerInfo.h

---
+++ PlayerInfo.h
@@ -0,0 +1,40 @@
+#pragma once
+#include"Mecro.h"
+
+class PlayerInfo//플레이어 정보
+{
+
+	int p_back = 5;//무르기 횟수
+	int turn = 1;
+	
+	int py = 20;//초기 시작 위치 y값 
+	int px = 20;//초기 시작 위치 x값
+
+
+
+
+public:
+	inline int retPx()
+	{
+		return(px);
+	}
+	inline int retPy()
+	{
+		return(py);
+	}
+	void Plusx(int i) {
+		px += i;
+	}
+	void Plusy(int i) {
+		py += i;
+	}
+	void Subx(int i) {
+		px -= i;
+	}
+	void Suby(int i) {
+		py -= i;
+	}
+
+
+
+};
