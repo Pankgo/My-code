d9d9262a280c7b9624b625ae66e20ee0c64bf930
vhswo <cba0898@gmail.com>
2023-01-17 15:47:45 +0900

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -5,11 +5,17 @@
 #include"GameSet.h"
 #include"PlayerInfo.h"
 
-class Game
+class MainGame
 {
+	int player1[75][75] = { 0, };
+	int player2[75][75] = { 0, };
 	
 public:
+
+	PlayerInfo *p1;
+	PlayerInfo *p2;
 	void Mainplay(int playerType);
-	Game();
+	void CMove(PlayerInfo *A,int move,int character);
+	MainGame();
 	
 };
