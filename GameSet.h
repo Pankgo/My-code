3aadcdde4f4faaefa372bdec613aac27212fc053
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-17 13:16:12 +0900

    Create GameSet.h

---
+++ GameSet.h
@@ -0,0 +1,23 @@
+#pragma once
+class GameSet//게임세팅
+{
+	int map_Width;
+	int map_Height;
+
+public:
+	void setGame()
+	{
+
+	}
+	int retWidth()//설정한 넓이 값 리턴
+	{
+		return map_Width;
+	}
+	int retHeight()//설정한 높이 값 리턴
+	{
+		return map_Height;
+	}
+	
+
+
+};
