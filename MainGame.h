c6de22355f279ebfe90d92e9689b6839b1d52124
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-18 13:16:25 +0900

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -2,20 +2,17 @@
 #include"Mecro.h"
 #include"MapDraw.h"
 #include"conio.h"
-#include"GameSet.h"
 #include"PlayerInfo.h"
+#include"Map.h"
 
 class MainGame
 {
-	int player1[75][75] = { 0, };
-	int player2[75][75] = { 0, };
+	
 	
 public:
 
-	PlayerInfo *p1;
-	PlayerInfo *p2;
-	void Mainplay(int playerType);
-	void CMove(PlayerInfo *A,int move,int character);
+	void Mainplay(int playerType,PlayerInfo *A, PlayerInfo *B);
+	void CMove(PlayerInfo *A,int move);
 	MainGame();
 	
 };
