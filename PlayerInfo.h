7c9d69c122ecf92b85dce4001497f89b342bba62
vhswo <cba0898@gmail.com>
2023-01-18 15:50:08 +0900

    Update PlayerInfo.h

--- PlayerInfo.h
+++ PlayerInfo.h
@@ -5,12 +5,9 @@ class PlayerInfo//플레이어 정보
 {
 
 	int p_back = 5;//무르기 횟수
+	int py = 10;//초기 시작 위치 y값 
+	int px = 10;//초기 시작 위치 x값
 	int turn = 1;
-	
-	int py = 20;//초기 시작 위치 y값 
-	int px = 20;//초기 시작 위치 x값
-	int stonepx[100] = { 0, };
-	int stonepy[100] = { 0, };
 
 
 
@@ -23,17 +20,29 @@ public:
 	{
 		return(py);
 	}
-	void Plusx(int i) {
+	void Plusx(int i) {//플레이어 x값 더하기
 		px += i;
 	}
-	void Plusy(int i) {
+	void Plusy(int i) {//플레이어 y값 더하기
 		py += i;
 	}
-	void Subx(int i) {
+	void Subx(int i) {//플레이어 x값 뺴기
 		px -= i;
 	}
-	void Suby(int i) {
+	void Suby(int i) {//플레이어 y값 빼기
 		py -= i;
+	}
+	void StartTurn()
+	{
+		turn = 0;
+	}
+	void EndTurn()
+	{
+		turn = 0;
+	}
+	int RetTurn()
+	{
+		return turn;
 	}
 
 
