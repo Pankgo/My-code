bf4b01f70ce099dad253d4acd56e89ad088ccba6
vhswo <cba0898@gmail.com>
2023-01-18 15:49:57 +0900

    Update Map.h

--- Map.h
+++ Map.h
@@ -3,20 +3,24 @@
 #include"Mecro.h"
 #include"MapDraw.h"
 #include"MainGame.h"
+#include"PlayerInfo.h"
+#include <vector>
 
 class Map
 {
 private:
 	int m_x=0;
 	int m_y=0;
-	int m_Width=0;
-	int m_Height=0;
+	int m_Width=20;
+	int m_Height=20;
+	std::vector<Point> stoneList;
 	PlayerInfo p1;
 	PlayerInfo p2;
 public: 
 	void UIDraw();
 	Map();
 	~Map();
+	void SaveStone();
 	
 };
 
