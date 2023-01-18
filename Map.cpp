573e3239e6094028062be1ed57c40332d0e4651f
vhswo <cba0898@gmail.com>
2023-01-18 15:49:52 +0900

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -10,22 +10,36 @@ enum Select
 	Setting,
 	Exit
 };
+enum Player
+{
+	F = 1,
+	S
+};
+enum Turn
+{
+	TStart=1,
+	TEnd = 0
+};
 
 Map::Map()
 {
-	MapDraw::GameMapDraw(0, 0, map_Width, map_Height);
+	MapDraw::GameMapDraw(0, 0, Map::m_Width, Map::m_Height);
+	stoneList = vector<Point>();
+	//stoneStack = std::stack<Point>();
+	//stoneList.rbegin
 }
 
 
 void Map::UIDraw()
 {
+		int turn = F;
 		int Select;
-		MapDraw::DrawMidText("1.게임 시작", map_Width, 5);
-		MapDraw::DrawMidText("2.Replay", map_Width, 7);
-		MapDraw::DrawMidText("3.게임 설정", map_Width, 9);
-		MapDraw::DrawMidText("4.종료", map_Width, 11);
+		MapDraw::DrawMidText("1.게임 시작", m_Width, 5);
+		MapDraw::DrawMidText("2.Replay", m_Width, 7);
+		MapDraw::DrawMidText("3.게임 설정", m_Width, 9);
+		MapDraw::DrawMidText("4.종료", m_Width, 11);
 		MapDraw::BoxDraw(14, 13, 7, 3);
-		MapDraw::gotoxy(map_Width, 14);
+		MapDraw::gotoxy(m_Width, 14);
 		cin >> Select;
 		switch (Select)
 		{
@@ -33,7 +47,21 @@ void Map::UIDraw()
 				while (1)
 				{
 					MapDraw::GameMapDraw(0, 0, Map::m_Width, Map::m_Height);
+					MapDraw::StoneDraw(stoneList);
+					MainGame::Mainplay(turn, &p1, &p2,stoneList);
+					if (turn == F && p1.RetTurn() == TEnd)
+					{
+						turn = S;
+						p1.RetTurn() == TStart;
+					}
+					else if(turn == S && p2.RetTurn() == TEnd)
+					{
+						turn = F;
+						p2.RetTurn() == TEnd;
+					}
+
 				}
+
 				break;
 			case Replay:
 				break;
