06b1a89f70067b21e794aae78b3d3d7d9db6a5d3
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-19 13:17:00 +0900

    Update PlayerInfo.h

--- PlayerInfo.h
+++ PlayerInfo.h
@@ -5,8 +5,8 @@ class PlayerInfo//플레이어 정보
 {
 
 	int p_back = 5;//무르기 횟수
-	int py = 10;//초기 시작 위치 y값 
-	int px = 10;//초기 시작 위치 x값
+	int py = 5;//초기 시작 위치 y값 
+	int px = 5;//초기 시작 위치 x값
 	int turn = 1;
 
 
@@ -20,21 +20,15 @@ public:
 	{
 		return(py);
 	}
-	void Plusx(int i) {//플레이어 x값 더하기
+	void Setx(int i) {//플레이어 x값 더하기
 		px += i;
 	}
-	void Plusy(int i) {//플레이어 y값 더하기
+	void Sety(int i) {//플레이어 y값 더하기
 		py += i;
-	}
-	void Subx(int i) {//플레이어 x값 뺴기
-		px -= i;
-	}
-	void Suby(int i) {//플레이어 y값 빼기
-		py -= i;
 	}
 	void StartTurn()
 	{
-		turn = 0;
+		turn = 1;
 	}
 	void EndTurn()
 	{
