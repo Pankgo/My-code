c67d9ac7cd8ae0dd9c43c83c6d4343833cd9b48e
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-01-26 12:24:47 +0900

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -14,6 +14,11 @@ public:
 	static void Mainplay(int playerType,PlayerInfo *A, PlayerInfo *B,vector<Point>*stoneList);
 	static void CMove(int playerType,PlayerInfo *A,int move,vector<Point>*stoneList);
 	static int WinCheck(vector<Point>stoneList,int wincheck[45][90],int x,int y);
+	static int WinRightCheck(int y, int x, int wincheck[45][90],int sequence);
+	static int WinLeftCheck(int y, int x, int wincheck[45][90],int sequence);
+	static int WinRightDiagonalCheck(int y, int x, int wincheck[45][90],int sequence);
+	static int WinLeftDiagonalCheck(int y, int x, int wincheck[45][90],int sequence);
+
 	MainGame();
 	
 };
