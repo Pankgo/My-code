e7a0a59a7c858b0ba6c6a274501312b300b27e51
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-04-13 11:41:35 +0900

    Update GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -3,6 +3,7 @@
 #include "DrawMap.h"
 #include "PlayerInfo.h"
 #include "WordInfo.h"
+#include "WordManager.h"
 #define MAXCOUNT 5
 
 enum Menu {
@@ -15,15 +16,15 @@ enum Menu {
 class GameManager
 {
 	DrawMap DrawMap;//전체적인 게임배경 그리는 클래스
+	vector<WordInfo> _curwordList;//현재 게임에 출력되는 단어리시트
 	int map_width = 60, map_height = 30;//게임의 전체적인 x,y
 	int startX = map_width - 13;
 	int startY = 10;
 	int inboxTextx = map_width - 5, inboxTexty = map_height / 2 + 7;//박스안의 텍스트 입력 위치
-	vector<WordInfo> _curwordList;//현재 게임에 출력되는 단어리시트
-	string wordList[76];
-	WordInfo wordInfo;
 	PlayerInfo playerInfo;
 	vector<PlayerInfo> _PlayerRank;
+	WordManager wordmanager;
+
 public:
 	void StartScreen();
 	void MainGame();
@@ -38,7 +39,15 @@ public:
 		DrawMap.Drawtext(startX, startY+2, "3.종료");
 
 	}
+	int GetmapY()
+	{
+		return map_height;
+	}
+	int GetmapX()
+	{
+		return map_width;
+	}
 	void Story();
-	void PrintStory(string group[26],int prtstart,int ptrend);
+	bool PrintStory(string group[26],int prtstart,int ptrend);
 };
 	
