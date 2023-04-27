fe062fa137d8831b05157a290fba90e1adbc71a1
tjdgus15 <jungyh3030@gmail.com>
2023-04-27 17:52:07 +0900

    Update Rank.h

--- Rank.h
+++ Rank.h
@@ -6,48 +6,97 @@ class _Rank
 {
 	DrawMap DrawMap;
 	vector<PlayerInfo> PlayerRank;
-	int checkstage =1;
-	int map_width = 60;
+	PlayerInfo playerInfo;
+	int map_width = 60, map_height = 30;
 public:
-	bool static compare_stage(PlayerInfo &A, PlayerInfo &B)
+	bool static compare_stage(PlayerInfo& A, PlayerInfo& B)//스테이지 비교함수
 	{
-		if (A.Getstage() < B.Getstage())
+		if (A < B)
 		{
-			return A.Getstage() < B.Getstage();
+			return true;
 		}
-		else
+		else if (A == B)
 		{
-			return A.Getstage() > B.Getstage();
+			return true;
 		}
-	}
-	bool static compare_point(PlayerInfo &A, PlayerInfo &B)
-	{
-		if (A.GetPoint() < B.GetPoint())
-		{
-			return A.GetPoint() < B.GetPoint();
-		}
-		else
-		{
-			return A.GetPoint() > B.GetPoint();
-		}
+		return false;
 	}
 	void setPlayerRank(PlayerInfo player)
 	{
 		PlayerRank.push_back(player);
 	}
-	void SortRank()
+	void SortRank() //랭크정렬
 	{
 		sort(PlayerRank.begin(), PlayerRank.end(), compare_stage);
-		sort(PlayerRank.begin(), PlayerRank.end(), compare_point);
 	}
-	void PrintRank()
+	void PrintRank() //랭크 출력
 	{
-		int y = 0;
-		for (vector<PlayerInfo>::iterator iter = PlayerRank.begin(); iter < PlayerRank.end(); iter++)//
+		int y = 0; 
+		int start_x =10;
+		int start_Rank = 0;
+		int select;
+		int i = 0;
+
+		DrawMap.Drawtext(start_x, 4, "이름",0);
+		DrawMap.Drawtext(map_width, 4, "스테이지",0);
+		DrawMap.Drawtext(map_width * 2 - 20, 4, "점수",0);
+		for (vector<PlayerInfo>::iterator iter = PlayerRank.end()-1; i < 8; iter--)//
 		{
-			DrawMap.Drawtext(map_width, 5 + y, iter->getName());
-			y++;
+			DrawMap.Drawtext(start_x-4, 5+ y, to_string(i+1), 0);
+			DrawMap.Drawtext(start_x - 3, 5 + y, "등 ", 0);
+			DrawMap.Drawtext(start_x, 5 + y, iter->getName(), 0);
+			DrawMap.Drawtext(map_width, 5 + y, to_string(iter->Getstage()), 0);
+			DrawMap.Drawtext(map_width * 2 - 20, 5 + y, to_string(iter->GetPoint()), 0);
+			y+=2;
+			i++;
 		}
+
+	}
+	void LoadRank() // 랭크데이터파일 불러오기
+	{
+		ifstream load;
+		string name;
+		int stage;
+		int point;
+		load.open("Rank.txt");
+
+		if (load.is_open())
+		{
+			while (!load.eof())
+			{
+				load >> name;
+				load >> stage;
+				load >> point;
+				playerInfo.Setname(name);
+				playerInfo.Setstage(stage);
+				playerInfo.ResetPoint(point);
+				setPlayerRank(playerInfo);
+			}
+		}
+
+		load.close();
+		//SortRank();
+		
+	}
+
+	void SaveRank() // 랭크데이터 파일로저장
+	{
+		ofstream save;
+		save.open("Rank.txt");
+		vector<PlayerInfo>::iterator iter = PlayerRank.begin();
+
+		while (iter != PlayerRank.end())
+		{
+			save << iter->getName();
+			save << " ";
+			save << iter->Getstage();
+			save << " ";
+			save << iter->GetPoint();
+			save << "\n";
+			iter++;
+		}
+
+
 	}
 	vector<PlayerInfo> GetRank()
 	{
