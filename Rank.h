5c1ec38d017384d04c25e743d649b1ede00e372c
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-05-01 14:55:31 +0900

    Update Rank.h

--- Rank.h
+++ Rank.h
@@ -27,7 +27,8 @@ public:
 	}
 	void SortRank() //랭크정렬
 	{
-		sort(PlayerRank.begin(), PlayerRank.end(), compare_stage);
+		sort(PlayerRank.begin(), PlayerRank.end(), compare_stage);//오름차순으로 정렬후
+		reverse(PlayerRank.begin(), PlayerRank.end());// reverse함수를 사용하여 내림차순으로 정렬
 	}
 	void PrintRank() //랭크 출력
 	{
@@ -35,20 +36,20 @@ public:
 		int start_x =10;
 		int start_Rank = 0;
 		int select;
-		int i = 0;
+		int i = 0;//8명까지만출력
 
 		DrawMap.Drawtext(start_x, 4, "이름",0);
 		DrawMap.Drawtext(map_width, 4, "스테이지",0);
 		DrawMap.Drawtext(map_width * 2 - 20, 4, "점수",0);
-		for (vector<PlayerInfo>::iterator iter = PlayerRank.end()-1; i < 8; iter--)//
+		for (vector<PlayerInfo>::iterator iter = PlayerRank.begin(); i < 8 && iter != PlayerRank.end(); y+=2,i++,iter++)//출력
 		{
 			DrawMap.Drawtext(start_x-4, 5+ y, to_string(i+1), 0);
 			DrawMap.Drawtext(start_x - 3, 5 + y, "등 ", 0);
 			DrawMap.Drawtext(start_x, 5 + y, iter->getName(), 0);
 			DrawMap.Drawtext(map_width, 5 + y, to_string(iter->Getstage()), 0);
 			DrawMap.Drawtext(map_width * 2 - 20, 5 + y, to_string(iter->GetPoint()), 0);
-			y+=2;
-			i++;
+			
+			
 		}
 
 	}
@@ -75,7 +76,6 @@ public:
 		}
 
 		load.close();
-		//SortRank();
 		
 	}
 
