cfa15e8985a5e07d414c56335f05e98637754d0b
tjdgus15 <jungyh3030@gmail.com>
2023-05-16 17:52:03 +0900

    Update Observer.h

--- News/Observer.h
+++ News/Observer.h
@@ -2,18 +2,36 @@
 #include "Master.h"
 
 class Observer {
-	std::string name;
-	std::string news;
-	std::string date;
 
-
-
-
+public:
+	virtual ~Observer() {}
+	virtual void UpdateInfo(std::string _news, std::string _date) abstract; //정보 업데이트 함수
+	virtual void PrintInfo() abstract; //정보 업데이트 함수
 
 };
 
 class customer : public Observer {
 
+	std::string name;
+	std::string news;
+	std::string date;
+public:
+	void UpdateInfo()
+	{
+		std::cout << "이름 입력 ";
+		std::cin >> name;
+	}
+	void UpdateInfo(std::string _news, std::string _date)
+	{
+		news = _news;
+		date = _date;
+	}
+
+	void PrintInfo()
+	{
+		std::cout << name << " " << news << std::endl;
+	}
 
 
-};
+
+}; 
