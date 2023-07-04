604ca59faeca1da806f4008224763e86492e6830
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-04 11:18:52 +0900

    Update GameManager.h

--- 새 폴더/GameManager.h
+++ 새 폴더/GameManager.h
@@ -3,17 +3,17 @@
 #include"Card.h"
 #define CARDMAX 12
 #define NORMAL 8 // 게임 난이도 설정
-
-
-class GameManager
-{
-	static GameManager* GameM;
-
 	enum page//화면종류
 	{
 		Start,
 		Game,
 	};
+
+class GameManager
+{
+	static GameManager* GameM;
+
+
 
 	BitMap* startbutton; // 시작버튼이미지
 	BitMap* BackGround; // 백그라운드이미지
@@ -26,7 +26,7 @@ class GameManager
 
 	int CurrStatue;//페이지상태
 
-	Card* cards;// [NORMAL] ;
+	std::vector<Card> cards;// [NORMAL] ;
 	
 public:
 	static GameManager* GetInstance()
@@ -36,13 +36,14 @@ public:
 		return GameM;
 	}
 	bool GameStartCheck(POINT point);//게임시작 눌렀는지 확인
-	bool PageCheck();//페이지 상태확인
+	page PageCheck();//페이지 상태확인
 	void PageDraw(HDC hdc);//현재 페이지 그리기
 	GameManager();
 	void GameP(HDC hdc);//게임페이지
 	void StartP(HDC hdc);//시작페이지
 	~GameManager();
-
+	bool ColliderCheck(POINT point);
 	void Init(HWND hWnd);
+	bool CardCheck(POINT point);
 };
 
