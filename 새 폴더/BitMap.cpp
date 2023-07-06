290f08631b23b96541c9614bafcfc3a95e7203bf
tjdgus15 <jungyh3030@gmail.com>
2023-07-06 17:50:09 +0900

    Update BitMap.cpp

--- 새 폴더/BitMap.cpp
+++ 새 폴더/BitMap.cpp
@@ -19,22 +19,30 @@ void BitMap::Init(HDC hdc,char* FileName)
 void BitMap::Draw(HDC hdc, int x, int y, int check)
 {
 	int height, width;
-	if (check == 1)//배경
+	switch (check)
 	{
-		height = m_Size.cy, width= m_Size.cx;
+		case 0:height = 100, width = 100; break; //시작버튼
+		case 1 :height = m_Size.cy, width = m_Size.cx; break; //배경
+		case 2: height = 100, width = 100; break;//다시시작버튼
+		default: height = 80, width = 80;//카드
+
 	}
-	else if (check == 0)//시작버튼
-	{
-		height = 100, width = 100;
-	}
-	else  if (check == 2) // 다시시작버튼
-	{
-		height = 100, width = 100;
-	}
-	else//카드
-	{
-		height = 80, width = 80;
-	}
+	//if (check == 1)//배경
+	//{
+	//	height = m_Size.cy, width= m_Size.cx;
+	//}
+	//else if (check == 0)//시작버튼
+	//{
+	//	height = 100, width = 100;
+	//}
+	//else  if (check == 2) // 다시시작버튼
+	//{
+	//	height = 100, width = 100;
+	//}
+	//else//카드
+	//{
+	//	height = 80, width = 80;
+	//}
 	StretchBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, SRCCOPY);
 }
 
