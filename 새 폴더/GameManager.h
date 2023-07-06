d632cd6c5ffa299da54b18020e37bd39f9b82e01
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-06 11:28:17 +0900

    Update GameManager.h

--- 새 폴더/GameManager.h
+++ 새 폴더/GameManager.h
@@ -3,47 +3,60 @@
 #include"Card.h"
 #define CARDMAX 12
 #define NORMAL 8 // 게임 난이도 설정
+#define WINDOWHEIGHT 640
+#define WINDOWWIDTH 490
 	enum page//화면종류
 	{
 		Start,
 		Game,
+		End
 	};
 
-class GameManager
-{
-	static GameManager* GameM;
-
-
-
-	BitMap* startbutton; // 시작버튼이미지
-	BitMap* BackGround; // 백그라운드이미지
-
-	RECT startRect;
-	RECT BackGroundRect;
-
-	int startb_x, startb_y;//시작버튼위치
-	int Back_X, Back_y;//백그라운드 위치
-
-	int CurrStatue;//페이지상태
-
-	std::vector<Card> cards;// [NORMAL] ;
-	
-public:
-	static GameManager* GetInstance()
+	class GameManager
 	{
-		if (GameM == NULL)
-			GameM = new GameManager;
-		return GameM;
-	}
-	bool GameStartCheck(POINT point);//게임시작 눌렀는지 확인
-	page PageCheck();//페이지 상태확인
-	void PageDraw(HDC hdc);//현재 페이지 그리기
-	GameManager();
-	void GameP(HDC hdc);//게임페이지
-	void StartP(HDC hdc);//시작페이지
-	~GameManager();
-	bool ColliderCheck(POINT point);
-	void Init(HWND hWnd);
-	bool CardCheck(POINT point);
-};
+		static GameManager* GameM;
+		struct Getxy //카드세팅에 필요한 xy좌표 구조체
+		{
+			int x, y;
+		};
+
+
+		BitMap* startbutton; // 시작버튼이미지
+		BitMap* BackGround; // 백그라운드이미지
+		BitMap* tryagain; // 트라이어겐버튼
+
+		RECT startRect;
+		RECT BackGroundRect;
+		RECT tryAgain;
+
+		int startb_x, startb_y;//시작버튼위치
+		int Back_X, Back_y;//백그라운드 위치
+		int try_x, try_y;
+		int cardcount = 0; //카드다 오픈되어있는지 확인
+		int CurrStatue;//페이지상태
+
+		std::vector<Card> cards;// [NORMAL] ;
+		std::vector<IMAGE>checkcards;//이미지 중복확인
+		std::vector<Getxy>xy;//2차원 백터를 이용한 xy좌표 저장백터
+		std::vector<Card>opencards;
+
+	public:
+		static GameManager* GetInstance()
+		{
+			if (GameM == NULL)
+				GameM = new GameManager;
+			return GameM;
+		}
+		void GameStart();//시작화면인경우 게임시작으로 상태변경
+		page PageCheck();//페이지 상태확인
+		void PageDraw(HDC hdc);//현재 페이지 그리기
+		GameManager();
+		void GameP(HDC hdc);//게임페이지
+		void StartP(HDC hdc);//시작페이지
+		void EndP(HDC hdc);
+		~GameManager();
+		bool ColliderCheck(POINT point,page page);
+		void Init(HWND hWnd);
+		void CardCheck(POINT point);
+	};
 
