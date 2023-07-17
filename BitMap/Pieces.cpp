cda4228141c5d04e13ef87ae75f5c15812aca90d
tjdgus15 <jungyh3030@gmail.com>
2023-07-17 17:29:10 +0900

    Update Pieces.cpp

---
+++ BitMap/Pieces.cpp
@@ -0,0 +1,23 @@
+#include "pieces.h"
+
+Chess::Chess()
+{
+
+}
+
+void Chess::Init(IMAGE Index, int x, int y)// 정보 입력
+{
+
+}
+void Chess::Draw(HDC hdc, CHECKIMAGE check)// 그리는 함수
+{
+
+}
+void Chess::UpdateState() //피스 이동가능 여부 업데이트
+{
+
+}
+bool Chess::ColliderCheck(POINT point) //기물과 마우스 위치 확인
+{
+
+}
