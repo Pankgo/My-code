42f9472ec6459c4b0a8e9089eab0432a3978d34f
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:11:29 +0900

    Update BitMapManager.cpp

--- BitMap/BitMapManager.cpp
+++ BitMap/BitMapManager.cpp
@@ -14,7 +14,7 @@ void BitMapManager::Init(HWND hWnd)
 	HDC hdc = GetDC(hWnd);
 	for (int i = IMAGE_START; i < IMAGE_END; i++)
 	{
-		sprintf_s(buf, "Res//0%d.bmp", i);
+		sprintf_s(buf, "RES//0%d.bmp", i);
 		m_parrBitMap[i].Init(hdc, buf);
 	}
 	ReleaseDC(hWnd, hdc);
