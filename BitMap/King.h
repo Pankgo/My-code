6c0dc034182f1f0099da14061039dd3815b145a9
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:12:06 +0900

    Update King.h

--- BitMap/King.h
+++ BitMap/King.h
@@ -1,10 +1,21 @@
-#include "Pieces.h"
+#include "pieces.h"
+#pragma once
 
-class King : public Chess
+class King : public _Chess
 {
 public:
-	virtual void Move()
+	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
 	{
+		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
+		{
+			switch (GetColor())
+			{
+			case black:
 
+				break;
+			default:
+				break;
+			}
+		}
 	}
 };
