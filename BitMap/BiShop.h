2bd314ce1225882bfb9113bb13861a25719260c2
tjdgus15 <jungyh3030@gmail.com>
2023-07-31 17:36:59 +0900

    Update BiShop.h

--- BitMap/BiShop.h
+++ BitMap/BiShop.h
@@ -3,19 +3,9 @@
 class BiShop : public _Chess
 {
 public:
-	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
+	void SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
 	{
-		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
-		{
-			switch (GetColor())
-			{
-			case black:
-
-				break;
-			default:
-				break;
-			}
-		}
+		
 	}
 
 };
