ff58e2f220e889e99f7267315506aee01dd1d4f7
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:12:08 +0900

    Update Pawn.h

--- BitMap/Pawn.h
+++ BitMap/Pawn.h
@@ -1,10 +1,22 @@
-#include "Pieces.h"
+#include "pieces.h"
+#pragma once
 
-class Pawn : public Chess
+
+class Pawn : public _Chess
 {
 public:
-	virtual void Move()
+	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
 	{
+		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
+		{
+			switch (GetColor())
+			{
+			case black:
 
+				break;
+			default:
+				break;
+			}
+		}
 	}
 };
