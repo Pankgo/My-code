b15503fa57925e0659d01d0ba9ee1b7e01828b26
tjdgus15 <jungyh3030@gmail.com>
2023-07-31 17:37:17 +0900

    Update Rook.h

--- BitMap/Rook.h
+++ BitMap/Rook.h
@@ -5,19 +5,9 @@ class Rook : public _Chess
 {
 public:
 	Rook() {};
-	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
+	void SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
 	{
-		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
-		{
-			switch (GetColor())
-			{
-			case black:
-
-				break;
-			default:
-				break;
-			}
-		}
+		
 	}
 	
 };
