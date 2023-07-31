41efd55875d9af9e075601107367ccf990e53541
tjdgus15 <jungyh3030@gmail.com>
2023-07-31 17:37:20 +0900

    Update Tiles.h

--- BitMap/Tiles.h
+++ BitMap/Tiles.h
@@ -27,5 +27,6 @@ public:
 	void Init(IMAGE Index,int x,int y);
 	void SetMoveableTiles();
 	int GetTx() { return t_x; }
+	bool ColliderCheck(POINT point); //기물과 마우스 위치 확인
 	int GetTy() { return t_y; }
 };
