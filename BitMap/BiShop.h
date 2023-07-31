7096bb7cabf10191c7ce42ede995328f05944f2d
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:11:19 +0900

    Update BiShop.h

--- BitMap/BiShop.h
+++ BitMap/BiShop.h
@@ -1,10 +1,21 @@
-#include "Pieces.h"
+#include "pieces.h"
 
-class BiShop : public Chess
+class BiShop : public _Chess
 {
 public:
-	virtual void Move()
+	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
 	{
+		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
+		{
+			switch (GetColor())
+			{
+			case black:
 
+				break;
+			default:
+				break;
+			}
+		}
 	}
+
 };
