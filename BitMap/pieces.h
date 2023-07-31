62670cdf75290064e0d0d7c1392f4b3a55ec3f8f
tjdgus15 <jungyh3030@gmail.com>
2023-07-31 17:37:13 +0900

    Update pieces.h

--- BitMap/pieces.h
+++ BitMap/pieces.h
@@ -26,10 +26,12 @@ class _Chess {
 	BitMap* pieces_Image; //피스이미지
 	RECT pices_rect; // 규격
 	int P_x, P_y; // 현재 피스 위치
+	bool selectpieces = false;
 
 public:
 	_Chess();
-	virtual void Move(std::vector <Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) = 0; // 피스 움직이는 함수(각 피스마다 움직일수있는 칸이 다름)
+	virtual void SetMove(Tiles tiles[], std::vector<_Chess*>pieces) = 0; // 피스 움직이는 함수(각 피스마다 움직일수있는 칸이 다름)
+	void Move(int T_x , int T_y);
 	void SetXY(int x, int y, IMAGE Index);// 정보 입력
 	void Draw(HDC hdc, CHECKIMAGE check); // 그리는 함수
 	bool ColliderCheck(POINT point); //기물과 마우스 위치 확인
@@ -37,6 +39,7 @@ public:
 	Color GetColor() { return color; }
 	int GetX() { return P_x; }
 	int GetY(){ return P_y; }
+	bool CheckSelect() { if (selectpieces) { return true; } return false; }
 
 
 
