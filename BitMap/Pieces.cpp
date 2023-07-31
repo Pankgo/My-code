f15fd595253580dc1feb6bbf9e67d384e0b4bc33
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:12:09 +0900

    Update Pieces.cpp

--- BitMap/Pieces.cpp
+++ BitMap/Pieces.cpp
@@ -1,23 +1,77 @@
 #include "pieces.h"
 
-Chess::Chess()
+_Chess::_Chess(){}
+
+void _Chess::Draw(HDC hdc, CHECKIMAGE check)// 그리는 함수
 {
+	pieces_Image->Draw(hdc, P_x, P_y, check);
 
 }
 
-void Chess::Init(IMAGE Index, int x, int y)// 정보 입력
+void _Chess::SetXY(int x, int y, IMAGE Index)
 {
-
+	switch (Index) // 후에 선택되었을때 피스 색깔 및 종류를 구분짓기 위해 세팅
+	{
+	case IMAGE_BlackKing :
+		color = _black;
+		type = _King;
+		break;
+	case IMAGE_WhiteKing:
+		color = _white;
+		type = _King;
+		break;
+	case IMAGE_BlackQueen:
+		color = _black;
+		type = _Queen;
+		break;
+	case IMAGE_WhiteQueen:
+		color = _black;
+		type = _Queen;
+	case IMAGE_BlackHorse:
+		color = _black;
+		type = _Knight;
+		break;
+	case IMAGE_WhiteHorse:
+		color = _white;
+		type = _Knight;
+		break;
+	case IMAGE_BlackBishop:
+		color = _black;
+		type = _Bishop;
+		break;
+	case IMAGE_WhiteBishop:
+		color = _white;
+		type = _Bishop;
+		break;
+	case IMAGE_BlackRook:
+		color = _black;
+		type = _Rook;
+		break;
+	case IMAGE_WhiteRook:
+		color = _white;
+		type = _Rook;
+		break;
+	case IMAGE_WhitePawn:
+		color = _white;
+		type = _Pawn;
+	case IMAGE_BlackPawn:
+		color = _black;
+		type = _Pawn;
+	}
+	pieces = Index;
+	pieces_Image = BitMapManager::GetInstance()->GetImage(Index);
+	P_x = x;
+	P_y = y;
+	pices_rect.left = x;
+	pices_rect.top = y;
+	pices_rect.right = pices_rect.left + 80;
+	pices_rect.bottom = pices_rect.top + 80;
 }
-void Chess::Draw(HDC hdc, CHECKIMAGE check)// 그리는 함수
-{
 
-}
-void Chess::UpdateState() //피스 이동가능 여부 업데이트
+bool _Chess::ColliderCheck(POINT point) //기물과 마우스 위치 확인
 {
-
-}
-bool Chess::ColliderCheck(POINT point) //기물과 마우스 위치 확인
-{
-
+	if (PtInRect(&pices_rect, point)) 
+	{ 
+		return true; 
+	};
 }
