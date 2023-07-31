a8fb2d2a0293569043ce5d08eae8ff2121a5339f
tjdgus15 <jungyh3030@gmail.com>
2023-07-31 17:37:15 +0900

    Update Queen.h

--- BitMap/Queen.h
+++ BitMap/Queen.h
@@ -6,18 +6,8 @@ class Queen : public _Chess
 {
 public:
 	Queen() {};
-	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
+	void SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
 	{
-		for (auto iter = tiles->begin(); iter < tiles->end(); iter++)
-		{
-			switch (GetColor())
-			{
-			case black:
-
-				break;
-			default:
-				break;
-			}
-		}
+		
 	}
 };
