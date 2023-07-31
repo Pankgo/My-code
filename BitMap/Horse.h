5d98a9dfc0328e4d48c6db5f93540b3270f2e5f3
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-07-31 11:12:04 +0900

    Update Horse.h

--- BitMap/Horse.h
+++ BitMap/Horse.h
@@ -1,10 +1,92 @@
-#include "Pieces.h"
+#include "pieces.h"
+#pragma once
 
-class Horse : public Chess
+
+
+class Horse : public _Chess
 {
+	struct xy {
+		int x, y;
+	};
+	std::vector<xy>horse;
+
 public:
-	virtual void Move()
+
+	void Move(std::vector<Tiles>* tiles, std::vector<std::shared_ptr<_Chess>>pieces) override
 	{
+		int curX = GetX();
+		int curY = GetY();
+		xy _xy;
+
+		for (int x=-2; x <3; x++) // 말 피스의 움직일수 있는 좌표 세팅;
+		{
+			_xy.x = curX + x * 80;
+			if (_xy.x >= 0 )
+			{
+				switch (x)
+				{
+				case 1:
+					_xy.y = curY - 160;
+					horse.push_back(_xy);
+					_xy.y = curY + 160;
+					horse.push_back(_xy);
+					break;
+				case -1:
+					_xy.y = curY - 160;
+					horse.push_back(_xy);
+					_xy.y = curY + 160;
+					horse.push_back(_xy);
+					break;
+				case -2:
+					_xy.y = curY - 80;
+					horse.push_back(_xy);
+					_xy.y = curY + 80;
+					horse.push_back(_xy);
+					break;
+				case 2:
+					_xy.y = curY - 80;
+					horse.push_back(_xy);
+					_xy.y = curY + 80;
+					horse.push_back(_xy);
+					break;
+				}
+			}
+			
+
+		}
+		int length = 0;
+		int maxSize = horse.size();
+		bool stop = false;
+		for (auto iter2 = horse.begin(); length< maxSize; length++)
+		{
+			for (auto iter = pieces.begin(); iter < pieces.end(); iter++)
+			{
+				if ((*iter)->GetX() == iter2->x && (*iter)->GetY() == iter2->y) // 반복문을 돌려 현재 게임판들의 피스들의 좌표와 
+				{
+					horse.erase(iter2);
+					maxSize = horse.size();
+					stop = true;
+					break;
+
+				}
+			}
+			if (!stop)
+			{
+				iter2++;
+			}
+		}
+
+		for (auto iter = horse.begin(); iter < horse.end(); iter++)
+		{
+			for (auto iter2 = tiles->begin(); iter2 < tiles->end(); iter2++)
+			{
+				if (iter->x == iter2->GetTx() && iter->y== iter2->GetTy()) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
+				{														//동일한 판들의 정보(범위 여부) 보여줌
+					iter2->SetMoveableTiles();
+				}
+			}
+		}
+
 
 	}
 };
