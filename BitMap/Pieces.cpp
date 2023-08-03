3960d2ad9d662bcba395e3648c8e73bf50882772
tjdgus15 <jungyh3030@gmail.com>
2023-08-03 17:35:56 +0900

    Update Pieces.cpp

--- BitMap/Pieces.cpp
+++ BitMap/Pieces.cpp
@@ -8,10 +8,22 @@ void _Chess::Draw(HDC hdc, CHECKIMAGE check)// 그리는 함수
 
 }
 
-void _Chess::Move(int T_x, int T_y) // 선택된 타일의 xy좌표를 가져와 피스 좌표 변환
+void _Chess::Move(int T_x, int T_y, Tiles tiles[64]) // 선택된 타일의 xy좌표를 가져와 피스 좌표 변환
 {
+	selectpieces = false;
 	P_x = T_x;
 	P_y = T_y;
+	pices_rect.left = T_x;
+	pices_rect.top = T_y;
+	pices_rect.right = pices_rect.left + 80;
+	pices_rect.bottom = pices_rect.top + 80;
+	for (int i = 0; i < 64; i++)
+	{
+		if (tiles[i].Getstate() == plusmoveable)
+		{
+			tiles[i].SetMoveableTiles();
+		}
+	}
 }
 
 void _Chess::SetXY(int x, int y, IMAGE Index)
@@ -60,9 +72,11 @@ void _Chess::SetXY(int x, int y, IMAGE Index)
 	case IMAGE_WhitePawn:
 		color = _white;
 		type = _Pawn;
+		break;
 	case IMAGE_BlackPawn:
 		color = _black;
 		type = _Pawn;
+		break;
 	}
 	pieces = Index;
 	pieces_Image = BitMapManager::GetInstance()->GetImage(Index);
@@ -83,3 +97,49 @@ bool _Chess::ColliderCheck(POINT point) //기물과 마우스 위치 확인
 	}
 	return false;
 }
+
+void _Chess::Catch(std::vector<_Chess*>pieces) // 상대피스잡을경우
+{
+
+}
+
+bool _Chess::CheckColor(int x, int y, std::vector<_Chess*> pieces)
+{
+	for (auto iter1 = pieces.begin(); iter1 < pieces.end(); iter1++) //타일설정
+	{
+		if (x == (*iter1)->GetX() && y == (*iter1)->GetY())// 해당 위치에 피스가 있을경우 리턴
+		{
+			if (GetColor() != (*iter1)->GetColor())
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
+bool _Chess::CheckPieces(int x, int y, std::vector<_Chess*> pieces)
+{
+	if (x < 0 || x > 560)
+	{
+		return false;
+	}
+	if (y < 0 || y > 560)
+	{
+		return false;
+	}
+	for (auto iter1 = pieces.begin(); iter1 < pieces.end(); iter1++) //타일설정
+	{
+		if (x == (*iter1)->GetX() && y == (*iter1)->GetY() )// 해당 위치에 피스가 있을경우 리턴
+		{
+			if (GetColor() != (*iter1)->GetColor())
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+	return true;
+}
+
+
