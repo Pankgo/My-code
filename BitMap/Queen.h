38354faf55c73a619127a266471ae02fb600590c
tjdgus15 <jungyh3030@gmail.com>
2023-08-03 17:36:01 +0900

    Update Queen.h

--- BitMap/Queen.h
+++ BitMap/Queen.h
@@ -4,10 +4,36 @@
 
 class Queen : public _Chess
 {
+	struct xy {
+		int x, y;
+		bool pass = true;
+	};
+
+	std::vector<xy> queen;
 public:
 	Queen() {};
-	void SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
+	bool SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
 	{
-		
+		int curY = GetY();//현재 퀸의 x좌표
+		int curX = GetX();//현재 퀸의 y좌표
+
+		xy newxy;
+
+
+
+		for(int x =0; x<8;x++)
+		{
+
+		}
+
+		for (int y = 0; y < 8; y++)
+		{
+
+		}
+
+
+		return true;
+		//대각선 타일 선정
+		// 십자가 타일선정
 	}
 };
