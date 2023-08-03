51a5c23f8b7787bbd83a21efcae99be2be738eae
tjdgus15 <jungyh3030@gmail.com>
2023-08-03 17:35:54 +0900

    Update Pawn.h

--- BitMap/Pawn.h
+++ BitMap/Pawn.h
@@ -7,20 +7,23 @@ class Pawn : public _Chess
 	
 
 public:
-	void SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
+	bool SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
 	{
 		int curY = GetY();
 		int curX = GetX();
 		switch (GetColor())
 		{
 		case _white:
+
 			if (curY == 80) // 초기 화이트폰 위치인지 확인
 			{
 				for (int i = 0; i < 64; i++)
 				{
-					if (tiles[i].GetTy() == curY + 80 || tiles[i].GetTy() == curY + 160) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
+
+					if (tiles[i].GetTx() == curX && tiles[i].GetTy() == curY + 80  || tiles[i].GetTx() == curX && tiles[i].GetTy() == curY + 160 ) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
 					{														//동일한 판들의 정보(범위 여부) 보여줌 , 처음 폰이 움직일때는 최대 2칸까지 이동가능
 						tiles[i].SetMoveableTiles();
+						
 					}
 				}
 			}
@@ -28,20 +31,23 @@ public:
 			{
 				for (int i = 0; i < 64; i++)
 				{
-					if (tiles[i].GetTy() == curY + 80 ) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
+					if (tiles[i].GetTy() == curY + 80 && tiles[i].GetTx() == curX) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
 					{														//동일한 판들의 정보(범위 여부) 보여줌
 						tiles[i].SetMoveableTiles();
 					}
 				}
 			}
+
+			break;
 		case _black:
 			if (curY == 480) // 초기 블랙폰 위치인지 확인
 			{
 				for (int i = 0; i < 64; i++)
 				{
-					if (tiles[i].GetTy() == curY - 80 || tiles[i].GetTy() == curY - 160) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
+					if (tiles[i].GetTx() == curX && tiles[i].GetTy() == curY - 80 || tiles[i].GetTx() == curX && tiles[i].GetTy() == curY - 160) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
 					{														//동일한 판들의 정보(범위 여부) 보여줌 , 처음 폰이 움직일때는 최대 2칸까지 이동가능
 						tiles[i].SetMoveableTiles();
+
 					}
 				}
 			}
@@ -49,14 +55,16 @@ public:
 			{
 				for (int i = 0; i < 64; i++)
 				{
-					if (tiles[i].GetTy() == curY - 80) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
+					if (tiles[i].GetTy() == curY - 80 && tiles[i].GetTx() == curX) // 반복문을 돌려 나이츠의 움직일수있는 판의 좌표와 
 					{														//동일한 판들의 정보(범위 여부) 보여줌
 						tiles[i].SetMoveableTiles();
 					}
 				}
 			}
+			
+			break;
 		}
 
-
+		return true;
 	}
 };
