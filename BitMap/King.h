07f088adfd0ed848b2448d4c7b7073ece8466108
tjdgus15 <jungyh3030@gmail.com>
2023-08-07 17:34:31 +0900

    Update King.h

--- BitMap/King.h
+++ BitMap/King.h
@@ -3,132 +3,76 @@
 
 class King : public _Chess
 {
-	struct xy {
-		int x, y;
-		bool pass = true;
-	};
-	xy king[8];
 public:
-	bool SetMove(Tiles tiles[64], std::vector<_Chess*>pieces) override
+	std::vector<POINT>  SetMove(std::vector<_Chess*>pieces) override
 	{
+
+		std::vector<POINT> moveable;
+
 		int curY = GetY();
 		int curX = GetX();
-		xy _newxy;
-
-		int kingcount = 0;
 
 		int x1 = curX + 80;
 		int x2 = curX - 80;
 		int y1 = curY + 80;
 		int y2 = curY - 80;
 
-
-		//대각선 세팅
-		switch (CheckPieces(x2, y2, pieces)) // 왼쪽 위 검사
+		for (int i = 0; i < 1; i++) //상하좌우 세팅
 		{
-		case true:
-			_newxy.x = x2;
-			_newxy.y = y2;
-			king[0] = _newxy;
-			break;
-		default:
-			kingcount++;
-			king[0].pass= false;
-		}
-		switch (CheckPieces(x1, y2, pieces))// 오른쪽 위 검사
-		{
-		case true:
-			_newxy.x = x1;
-			_newxy.y = y2;
-			king[1] = _newxy;
-			break;
-		default:
-			king[1].pass = false;
-			kingcount++;
-		}
-		switch ( CheckPieces(x2, y1, pieces)) // 왼쪽 아래 검사
-		{
-		case true:
-			_newxy.x = x2;
-			_newxy.y = y1;
-			king[2] = _newxy;
-			break;
-		default:
-			king[2].pass = false;
-			kingcount++;
-		}
-		switch (CheckPieces(x1, y1, pieces))// 오른쪽 아래 검사
-		{
-		case true:
-			_newxy.x = x1;
-			_newxy.y = y1;
-			king[3] = _newxy ;
-			break;
-		default:
-			king[3].pass = false;
-			kingcount++;
-		}
-		//상하좌우
-		switch (CheckPieces(curX, y2, pieces)) // 상
-		{
-		case true:
-			_newxy.x = curX;
-			_newxy.y = y2;
-			king[4] = _newxy;
-			break;
-		default:
-			king[4].pass = false;
-			kingcount++;
-		}
-		switch (CheckPieces(curX, y1, pieces))// 하
-		{
-		case true:
-			_newxy.x = curX;
-			_newxy.y = y1;
-			king[5] = _newxy;
-			break;
-		default:
-			king[5].pass = false;
-			kingcount++;
-		}
-		switch (CheckPieces(x2, curY, pieces)) // 좌
-		{
-		case true:
-			_newxy.x = x2;
-			_newxy.y = curY;
-			king[6] = _newxy;
-			break;
-		default:
-			king[6].pass = false;
-			kingcount++;
-		}
-		switch (CheckPieces(x1, curY, pieces))// 우
-		{
-		case true:
-			_newxy.x = x1;
-			_newxy.y = curY;
-			king[7] = _newxy;
-			break;
-		default:
-			king[7].pass = false;
-			kingcount++;
-		}
-
-		if (kingcount == 8)
-		{
-			return false;
-		}
-		for (int i = 0; i < 8; i++) //타일설정
-		{
-			for (int iter = 0; iter < 64; iter++)
+			switch (i)
 			{
-				if ( king[i].pass == true && king[i].x == tiles[iter].GetTx() && king[i].y == tiles[iter].GetTy())
+			case 0:
+				if (CheckPieces(curX - 80, curY, pieces))
 				{
-					tiles[iter].SetMoveableTiles();
+					moveable.push_back({ curX - 80, curY });
 				}
+				if (CheckPieces(curX + 80, curY, pieces))
+				{
+					moveable.push_back({ curX + 80, curY });
+				}
+				break;
+			case 1:
+				if (CheckPieces(curX, curY+80, pieces))
+				{
+					moveable.push_back({ curX, curY +80});
+				}
+				if (CheckPieces(curX, curY, pieces))
+				{
+					moveable.push_back({ curX , curY - 80});
+				}
+				break;
 			}
 		}
 
-		return true;
+		for (int i = 0; i < 1; i++) //대각선 세팅
+		{
+			switch (i)
+			{
+			case 0:
+				if (CheckPieces(curX - 80, curY + 80, pieces))
+				{
+					moveable.push_back({ curX - 80, curY + 80 });
+				}
+				if (CheckPieces(curX - 80, curY - 80, pieces))
+				{
+					moveable.push_back({ curX - 80, curY - 80});
+				}
+				break;
+			case 1:
+				if (CheckPieces(curX + 80, curY + 80, pieces))
+				{
+					moveable.push_back({ curX + 80, curY + 80 });
+				}
+				if (CheckPieces(curX  + 80, curY-80, pieces))
+				{
+					moveable.push_back({ curX + 80 , curY - 80 });
+				}
+				break;
+			}
+		}
+		
+
+
+		return moveable;
 	}
 };
