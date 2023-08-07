fbc9a8331aba9868d274520b22ee18a3d78f9b36
tjdgus15 <jungyh3030@gmail.com>
2023-08-07 17:34:37 +0900

    Update pieces.h

--- BitMap/pieces.h
+++ BitMap/pieces.h
@@ -28,9 +28,12 @@ class _Chess {
 	int P_x, P_y; // 현재 피스 위치
 	bool selectpieces = false;
 
+protected:
+	bool firstMove = false;
+
 public:
 	_Chess();
-	virtual bool SetMove(Tiles tiles[], std::vector<_Chess*>pieces) = 0; // 피스 움직이는 함수(각 피스마다 움직일수있는 칸이 다름)
+	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) = 0; // 피스 움직이는 함수(각 피스마다 움직일수있는 칸이 다름)
 	bool Move(int T_x , int T_y, Tiles tiles[64], std::vector<_Chess*> *pieces);
 	void SetXY(int x, int y, IMAGE Index);// 정보 입력
 	void Draw(HDC hdc, CHECKIMAGE check); // 그리는 함수
