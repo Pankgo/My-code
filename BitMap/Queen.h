47184146f428dcf456863bc46246e88a7c6d35b9
tjdgus15 <jungyh3030@gmail.com>
2023-08-08 17:35:57 +0900

    Update Queen.h

--- BitMap/Queen.h
+++ BitMap/Queen.h
@@ -1,196 +1,106 @@
-#include "pieces.h"
 #pragma once
 
+#include "pieces.h"
 
 class Queen : public _Chess
 {
-	struct xy {
-		int x, y;
-		bool pass = true;
-	};
-
-	std::vector<xy> queen;
 public:
 	Queen() {};
-	std::vector<POINT>  SetMove( std::vector<_Chess*>pieces) override
+	virtual std::vector<POINT>  SetMove(std::vector<_Chess*>pieces) override
 	{
-		int curY = GetY();//현재 퀸의 x좌표
-		int curX = GetX();//현재 퀸의 y좌표
 
-		xy newxy;
+		std::vector<POINT> moveable;
 
-		int count = 1;//한칸씩 대각선 좌표 변화(x + count*80, y - count* 80 or x - count * 80 ; y + count * 80)
-		bool plus1 = true; //왼쪽 위 대각선
-		bool plus2 = true; //오른쪽 위대각선
-		bool plus3 = true; //왼쪽 아래 대각선
-		bool plus4 = true; //오른쪽 아래 대각선
+		int curY = GetY();
+		int curX = GetX();
 
-		int x1 = 0;
-		int y1 = 0;
-		int x2 = 0;
-		int y2 = 0;
+		bool down = true; // 상하좌우
+		bool up = true;
+		bool right = true;
+		bool left = true;
 
-		//대각선처리
-		for (int count = 1; plus1 == true || plus2 == true || plus3 == true || plus4 == true; count++) // 맵크기에서 벗어나면안됨
+		bool left1 = true; // 대각선
+		bool left2 = true;
+		bool right1 = true;
+		bool right2 = true;
+
+
+		for (int count = 1; count < 8; count++)
 		{
-			x1 = curX + count * 80;
-			x2 = curX - count * 80;
-			y1 = curY + count * 80;
-			y2 = curY - count * 80;
-			switch (plus1 && CheckPieces(x2, y2, pieces)) // 왼쪽 위 검사
+			if (CheckPieces(curX, curY + count * 80, pieces) && down) // 하
 			{
-			case true:
-				newxy.x = x2;
-				newxy.y = y2;
-				queen.push_back(newxy);
-				if (CheckColor(x2, y2, pieces))
-				{
-					plus1 = false;
-				}
-				break;
-			case false:
-				plus1 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus2 && CheckPieces(x1, y2, pieces))// 오른쪽 위 검사
+			else
 			{
-			case true:
-				newxy.x = x1;
-				newxy.y = y2;
-				queen.push_back(newxy);
-				if (CheckColor(x1, y2, pieces))
-				{
-					plus2 = false;
-				}
-				break;
-			case false:
-				plus2 = false;
-				break;
+				down = false;
 			}
-			switch (plus3 && CheckPieces(x2, y1, pieces)) // 왼쪽 아래 검사
+			if (CheckPieces(curX, curY - count * 80, pieces) && up) // 상
 			{
-			case true:
-				newxy.x = x2;
-				newxy.y = y1;
-				queen.push_back(newxy);
-				if (CheckColor(x2, y1, pieces))
-				{
-					plus3 = false;
-				}
-				break;
-			case false:
-				plus3 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus4 && CheckPieces(x1, y1, pieces))// 오른쪽 아래 검사
+			else
 			{
-			case true:
-				newxy.x = x1;
-				newxy.y = y1;
-				queen.push_back(newxy);
-				if (CheckColor(x1, y1, pieces))
-				{
-					plus4 = false;
-				}
-				break;
-			case false:
-				plus4 = false;
-				break;
+				up = false;
 			}
-
-		}
-		//상하좌우 세팅
-		count = 1; plus1 = plus2 = plus3 = plus4 = true;
-
-		for (int count = 1; plus1 == true || plus2 == true || plus3 == true || plus4 == true; count++) // 맵크기에서 벗어나면안됨
-		{
-			x1 = curX + count * 80;
-			x2 = curX - count * 80;
-			y1 = curY + count * 80;
-			y2 = curY - count * 80;
-			switch (plus1 && CheckPieces(curX, y2, pieces)) // 상
+			if (CheckPieces(curX + 80 * count, curY, pieces) && right) // 우
 			{
-			case true:
-				newxy.x = curX;
-				newxy.y = y2;
-				queen.push_back(newxy);
-				if (CheckColor(curX,y2, pieces))
-				{
-					plus1 = false;
-				}
-				break;
-			case false:
-				plus1 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus2 && CheckPieces(curX, y1, pieces))// 하
+			else
 			{
-			case true:
-				newxy.x = curX;
-				newxy.y = y1;
-				queen.push_back(newxy);
-				if (CheckColor(curX, y1, pieces))
-				{
-					plus2 = false;
-				}
-				break;
-			case false:
-				plus2 = false;
-				break;
+				right = false;
 			}
-			switch (plus3 && CheckPieces(x2, curY, pieces)) // 좌
+			if (CheckPieces(curX - 80 * count, curY, pieces) && left) //좌
 			{
-			case true:
-				newxy.x = x2;
-				newxy.y = curY;
-				queen.push_back(newxy);
-				if (CheckColor(x2, curY, pieces))
-				{
-					plus3 = false;
-				}
-				break;
-			case false:
-				plus3 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus4 && CheckPieces(x1, curY, pieces))// 우
+			else
 			{
-			case true:
-				newxy.x = x1;
-				newxy.y = curY;
-				queen.push_back(newxy);
-				if (CheckColor(x1, curY, pieces))
-				{
-					plus4 = false;
-				}
-				break;
-			case false:
-				plus4 = false;
-				break;
+				left = false;
 			}
 
 		}
 
-		if (queen.size() == 0)
-		{
-			return false;
-		}
 
 
-		for (auto iter1 = queen.begin(); iter1 < queen.end(); iter1++) //타일설정
+		for (int count = 1; count < 8; count++)
 		{
-			for (auto iter2 = 0; iter2 < 64; iter2++)
+			if (CheckPieces(curX - 80 * count, curY + count * 80, pieces) && left1) // (-,+) 대각선
 			{
-				if (iter1->x == tiles[iter2].GetTx() && iter1->y == tiles[iter2].GetTy())
-				{
-					tiles[iter2].SetMoveableTiles();
-				}
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
+			else
+			{
+				left1 = false;
+			}
+			if (CheckPieces(curX - 80 * count, curY - count * 80, pieces) && left2) // (-,-) 대각선
+			{
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
+			}
+			else
+			{
+				left2 = false;
+			}
+			if (CheckPieces(curX + 80 * count, curY - count * 80, pieces) && right1) // (+,-) 대각선
+			{
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
+			}
+			else
+			{
+				right1 = false;
+			}
+			if (CheckPieces(curX + 80 * count, curY + count * 80, pieces) && right2) // (+,+) 대각선
+			{
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
+			}
+			else
+			{
+				right2 = false;
+			}
+
 		}
 
-		queen.clear();
-		return true;
-		
-		// 십자가 타일선정
+		return moveable;
 	}
 };
