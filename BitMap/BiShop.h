d9de359ae0038ff0ff9ddbf691d0d428cc397957
tjdgus15 <jungyh3030@gmail.com>
2023-08-08 17:35:34 +0900

    Update BiShop.h

--- BitMap/BiShop.h
+++ BitMap/BiShop.h
@@ -1,124 +1,69 @@
+#pragma once
+
 #include "pieces.h"
 
 class BiShop : public _Chess
 {
 
-	struct xy {
-		int x, y;
-		bool pass = true;
-	}; 
-	std::vector<xy> bishop;
-
 public:
-	std::vector<POINT>  SetMove(std::vector<_Chess*>pieces) override
+	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) override
 	{
+		std::vector<POINT> moveable;
+
 		int curY = GetY();
 		int curX = GetX();
-		
-		int count = 1;//한칸씩 대각선 좌표 변화(x + count*80, y - count* 80 or x - count * 80 ; y + count * 80)
-		bool plus1 = true; //왼쪽 위 대각선
-		bool plus2 = true; //오른쪽 위대각선
-		bool plus3 = true; //왼쪽 아래 대각선
-		bool plus4 = true; //오른쪽 아래 대각선
-		
-		int x1 = 0;
-		int y1 = 0;
-		int x2 = 0;
-		int y2 = 0;
 
-		xy newxy;
-		newxy.pass = true;
+		int x1 = curX + 80;
+		int x2 = curX - 80;
+		int y1 = curY + 80;
+		int y2 = curY - 80;
+
+		bool left1 = true;
+		bool left2 = true;
+		bool right1 = true;
+		bool right2 = true;
 
 
 
-		for(int count = 1; plus1 == true || plus2 == true || plus3 == true || plus4 == true; count++) // 맵크기에서 벗어나면안됨
+		int count = 1;
+
+		for (int count = 1; count < 8; count++)
 		{
-			x1 = curX + count * 80;
-			x2 = curX - count * 80;
-			y1 = curY + count * 80;
-			y2 = curY - count * 80;
-			switch (plus1&&CheckPieces(x2, y2, pieces)) // 왼쪽 위 검사
+			if (CheckPieces(curX - 80 * count, curY + count * 80, pieces) && left1) // (-,+) 대각선
 			{
-			case true:
-				newxy.x = x2;
-				newxy.y = y2;
-				bishop.push_back(newxy);
-				if (CheckColor(x2, y2, pieces))
-				{
-					plus1 = false;
-				}
-				break;
-			case false:
-				plus1 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus2 && CheckPieces(x1, y2, pieces))// 오른쪽 위 검사
+			else
 			{
-			case true:
-				newxy.x = x1;
-				newxy.y = y2;
-				bishop.push_back(newxy);
-				if (CheckColor(x1, y2, pieces))
-				{
-					plus2 = false;
-				}
-				break;
-			case false:
-				plus2 = false;
-				break;
+				left1 = false;
 			}
-			switch (plus3 && CheckPieces(x2, y1, pieces)) // 왼쪽 아래 검사
+			if (CheckPieces(curX - 80 * count, curY - count * 80, pieces) && left2) // (-,-) 대각선
 			{
-			case true:
-				newxy.x = x2;
-				newxy.y = y1;
-				bishop.push_back(newxy);
-				if (CheckColor(x2, y1, pieces))
-				{
-					plus3 = false;
-				}
-				break;
-			case false:
-				plus3 = false;
-				break;
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
 			}
-			switch (plus4 && CheckPieces(x1, y1, pieces))// 오른쪽 아래 검사
+			else
 			{
-			case true:
-				newxy.x = x1;
-				newxy.y = y1;
-				bishop.push_back(newxy);
-				if (CheckColor(x1, y1, pieces))
-				{
-					plus4 = false;
-				}
-				break;
-			case false:
-				plus4 = false;
-				break;
+				left2 = false;
 			}
-
+			if (CheckPieces(curX + 80 * count, curY - count * 80, pieces) && right1) // (+,-) 대각선
+			{
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
+			}
+			else
+			{
+				right1 = false;
+			}
+			if (CheckPieces(curX + 80 * count, curY + count * 80, pieces) && right2) // (+,+) 대각선
+			{
+				moveable.push_back({ curX - 80 * count, curY + count * 80 });
+			}
+			else
+			{
+				right2 = false;
+			}
 		}
 
-		if (bishop.size() == 0)
-		{
-			return false;
-		}
 
-		for (auto iter1 = bishop.begin(); iter1 < bishop.end(); iter1++) //타일설정
-		{
-			for (auto iter2 = 0; iter2 < 64; iter2++)
-			{
-				if (iter1->x == tiles[iter2].GetTx() && iter1->y == tiles[iter2].GetTy())
-				{
-					tiles[iter2].SetMoveableTiles();
-				}
-			}
-		}
-		bishop.clear();
-		return true;
+		return moveable;
 	}
-
-
-
 };
