95e70a92f10be6f844f0f6ceeb1650f8cba09fb5
tjdgus15 <jungyh3030@gmail.com>
2023-08-14 17:53:25 +0900

    Update Pawn.h

--- BitMap/Pawn.h
+++ BitMap/Pawn.h
@@ -5,12 +5,56 @@
 class Pawn : public _Chess
 {
 	bool firstMove = true;
-
 public:
+	virtual void Set(int x, int y) override
+	{
+		if (y == 80)
+		{
+			SetXY(x, y, IMAGE_WhitePawn,_white);
+		}
+		else
+		{
+			SetXY(x, y, IMAGE_BlackPawn, _black);
+		}
+	}
 	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) override
 	{
-		std::vector<POINT> moveable;
+		/*std::vector<POINT> moveable;
 		
+		int curY = GetY();
+		int curX = GetX();
+
+		int x1 = curX + 80;
+		int x2 = curX - 80;
+		int y1 = curY + 80;
+		int y2 = curY - 80;
+
+		int forward = GetColor() ? 1 : -1;
+		const int size = firstMove ? 1 : 2;
+		for (int i = 0; size > i; i++)
+		{
+			if (CheckPieces(x2, curY + forward * 80 * (i + 1), pieces))
+			{
+				moveable.push_back({ x2, curY + forward * 80 * (i + 1) });
+				continue;
+			}
+
+			break;
+		}
+
+		if (CheckPieces(curX + 80, curY + forward * 80, pieces))
+		{
+			moveable.push_back({ curX + 80, curY + forward * 80 });
+		}
+		if (CheckPieces(curX - 80, curY + forward * 80, pieces))
+		{
+			moveable.push_back({ curX - 80, curY + forward * 80 });
+		}
+
+		
+		return moveable;*/
+		std::vector<POINT> moveable;
+
 		int curY = GetY();
 		int curX = GetX();
 
@@ -29,16 +73,14 @@ public:
 			break;
 		}
 
-		if (CheckPieces(curX + 80, curY + forward * 80, pieces) )
+		if (CheckPieces(curX + 80, curY + forward * 80, pieces))
 		{
 			moveable.push_back({ curX + 80, curY + forward * 80 });
 		}
-		if (CheckPieces(curX - 80, curY + forward * 80, pieces) )
+		if (CheckPieces(curX - 80, curY + forward * 80, pieces))
 		{
 			moveable.push_back({ curX - 80, curY + forward * 80 });
 		}
-
-		
 		return moveable;
 	}
 };
