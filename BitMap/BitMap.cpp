586d617285107284f4d982440e72b02ee35390d3
tjdgus15 <jungyh3030@gmail.com>
2023-08-14 17:53:08 +0900

    Update BitMap.cpp

--- BitMap/BitMap.cpp
+++ BitMap/BitMap.cpp
@@ -11,51 +11,57 @@ void BitMap::Init(HDC hdc,char* FileName)
 	MemDC = CreateCompatibleDC(hdc);
 	m_BitMap = (HBITMAP)LoadImageA(NULL, FileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_DEFAULTSIZE | LR_LOADFROMFILE);
 	alphaBitmap = (HBITMAP)LoadImage(NULL, L"RES//017.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-	OldBitmap=(HBITMAP)SelectObject(MemDC, m_BitMap);
 	BITMAP BitMap_Info;
 	GetObject(m_BitMap, sizeof(BitMap_Info), &BitMap_Info);
 	m_Size.cx = BitMap_Info.bmWidth;
 	m_Size.cy = BitMap_Info.bmHeight;
 }
-void BitMap::Draw(HDC hdc, int x, int y, int check,bool moveabletiles)
+void BitMap::Stretch(HDC hdc, int x, int y,int check)
 {
-
 	int height, width;
-	switch (check)
+	if (check == Start || check == Try)
 	{
-	case Start:height = 100, width = 100; break; //시작버튼
-	case Background:height = m_Size.cy, width = m_Size.cx;  break; //배경
-	case Try: height = 100, width = 100;  break;//다시시작버튼
-	case Tile: height = 80, width = 80; break; // 타일
-	case Pieces: height = 80, width = 80;  break;
+		height = 100; width = 100;
 	}
-
-	switch (check)
+	else
 	{
-	case Pieces:
-		TransparentBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, RGB(255, 0, 255));
-		break;
-	case Tile:
-		if (moveabletiles)
-		{
-			SelectObject(MemDC, alphaBitmap);
-			BLENDFUNCTION bf;
-			ZeroMemory(&bf, sizeof(bf));
-			bf.SourceConstantAlpha = 100; // 원하는 값(0 ~ 255)
-			AlphaBlend(hdc, x, y, width, height, MemDC, 0, 0, width, height, bf);
-		}
-		else
-		{
-			SelectObject(MemDC, m_BitMap);
-			StretchBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, SRCCOPY);
-		}
-		break;
-	default:
-		StretchBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, SRCCOPY);
+		height = 80; width = 80;
 	}
+	SelectObject(MemDC, m_BitMap);
+	StretchBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, SRCCOPY);
 
 }
-	
+void BitMap::Trans(HDC hdc, int x, int y,int check)
+{
+	int height, width;
+	if (check == Start || check == Try)
+	{
+		height = 100; width = 100;
+	}
+	else
+	{
+		height = 80; width = 80;
+	}
+	SelectObject(MemDC, m_BitMap);
+	TransparentBlt(hdc, x, y, width, height, MemDC, 0, 0, m_Size.cx, m_Size.cy, RGB(255, 0, 255));
+}
+void BitMap::Alpha(HDC hdc, int x, int y,int check)
+{
+	int height, width;
+	if (check == Start || check == Try)
+	{
+		height = 100; width = 100;
+	}
+	else
+	{
+		height = 80; width = 80;
+	}
+	SelectObject(MemDC, alphaBitmap);
+	BLENDFUNCTION bf;
+	ZeroMemory(&bf, sizeof(bf));
+	bf.SourceConstantAlpha = 100; // 원하는 값(0 ~ 255)
+	AlphaBlend(hdc, x, y, width, height, MemDC, 0, 0, width, height, bf);
+}
 
 BitMap::~BitMap()
 {
