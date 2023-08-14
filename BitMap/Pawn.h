cb4381bc40eb1705aacb58df786ee173d11702d1
Pankgo <98949106+Pankgo@users.noreply.github.com>
2023-08-14 09:25:05 +0900

    Update Pawn.h

--- BitMap/Pawn.h
+++ BitMap/Pawn.h
@@ -4,6 +4,7 @@
 
 class Pawn : public _Chess
 {
+	bool firstMove = true;
 
 public:
 	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) override
@@ -13,29 +14,26 @@ public:
 		int curY = GetY();
 		int curX = GetX();
 
-		int x1 = curX + 80;
-		int x2 = curX - 80;
-		int y1 = curY + 80;
-		int y2 = curY - 80;
 
 		int forward = GetColor() ? 1 : -1;
-		const int size = firstMove ? 1 : 2;
+		const int size = firstMove ? 2 : 1;
+		firstMove = false;
 		for (int i = 0; size > i; i++)
 		{
-			if (CheckPieces(x2, curY + forward * 80 * (i + 1), pieces))
+			if (CheckPieces(curX, curY + forward * 80 * (i + 1), pieces) == true)
 			{
-				moveable.push_back({ x2, curY + forward * 80 * (i + 1) });
+				moveable.push_back({ curX, curY + forward * 80 * (i + 1) });
 				continue;
 			}
 
 			break;
 		}
 
-		if (CheckPieces(curX + 80, curY + forward * 80, pieces))
+		if (CheckPieces(curX + 80, curY + forward * 80, pieces) )
 		{
 			moveable.push_back({ curX + 80, curY + forward * 80 });
 		}
-		if (CheckPieces(curX - 80, curY + forward * 80, pieces))
+		if (CheckPieces(curX - 80, curY + forward * 80, pieces) )
 		{
 			moveable.push_back({ curX - 80, curY + forward * 80 });
 		}
