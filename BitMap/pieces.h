97c82d21ad6c890bc80fcd2c9f28ea5e84d242c7
tjdgus15 <jungyh3030@gmail.com>
2023-08-14 17:53:28 +0900

    Update pieces.h

--- BitMap/pieces.h
+++ BitMap/pieces.h
@@ -20,22 +20,25 @@ enum _piecesType
 
 class _Chess {
 
+	HDC MemDC;
+	BitMap* pieces_Image; //피스이미지
+	HBITMAP m_BitMap;
+	SIZE m_Size;
 	Color color;
 	_piecesType type;
 	IMAGE pieces; //피스 구분
-	BitMap* pieces_Image; //피스이미지
 	RECT pices_rect; // 규격
 	int P_x, P_y; // 현재 피스 위치
 	bool selectpieces = false;
+	int height = 80, width = 80;
 
-//protected:
-//	bool firstMove = false;
 
 public:
 	_Chess();
 	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) = 0; // 피스 움직이는 함수(각 피스마다 움직일수있는 칸이 다름)
+	virtual void Set( int x, int y) = 0;
 	bool Move(int T_x , int T_y, Tiles tiles[64], std::vector<_Chess*> *pieces);
-	void SetXY(int x, int y, IMAGE Index);// 정보 입력
+	void SetXY(int x, int y, IMAGE index,Color _color, _piecesType _type);// 정보 입력
 	void Draw(HDC hdc, CHECKIMAGE check); // 그리는 함수
 	bool ColliderCheck(POINT point,int color); //기물과 마우스 위치 확인
 	_piecesType GetType() { return type; }
