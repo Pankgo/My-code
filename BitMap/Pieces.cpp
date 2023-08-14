4b3ada32c9ca7a80b55a48e022e63c8e89b710fb
tjdgus15 <jungyh3030@gmail.com>
2023-08-14 17:53:27 +0900

    Update Pieces.cpp

--- BitMap/Pieces.cpp
+++ BitMap/Pieces.cpp
@@ -4,8 +4,7 @@ _Chess::_Chess(){}
 
 void _Chess::Draw(HDC hdc, CHECKIMAGE check)// 그리는 함수
 {
-	pieces_Image->Draw(hdc, P_x, P_y, check,false);
-
+	pieces_Image->Trans(hdc, P_x, P_y, check);
 }
 
 bool _Chess::Move(int T_x, int T_y, Tiles tiles[64], std::vector<_Chess*> *pieces) // 선택된 타일의 xy좌표를 가져와 피스 좌표 변환
@@ -39,67 +38,18 @@ bool _Chess::Move(int T_x, int T_y, Tiles tiles[64], std::vector<_Chess*> *piece
 	return false;
 }
 
-void _Chess::SetXY(int x, int y, IMAGE Index)
+void _Chess::SetXY(int x, int y,IMAGE index,Color _color, _piecesType _type)
 {
-	switch (Index) // 후에 선택되었을때 피스 색깔 및 종류를 구분짓기 위해 세팅
-	{
-	case IMAGE_BlackKing :
-		color = _black;
-		type = _King;
-		break;
-	case IMAGE_WhiteKing:
-		color = _white;
-		type = _King;
-		break;
-	case IMAGE_BlackQueen:
-		color = _black;
-		type = _Queen;
-		break;
-	case IMAGE_WhiteQueen:
-		color = _white;
-		type = _Queen;
-		break;
-	case IMAGE_BlackHorse:
-		color = _black;
-		type = _Knight;
-		break;
-	case IMAGE_WhiteHorse:
-		color = _white;
-		type = _Knight;
-		break;
-	case IMAGE_BlackBishop:
-		color = _black;
-		type = _Bishop;
-		break;
-	case IMAGE_WhiteBishop:
-		color = _white;
-		type = _Bishop;
-		break;
-	case IMAGE_BlackRook:
-		color = _black;
-		type = _Rook;
-		break;
-	case IMAGE_WhiteRook:
-		color = _white;
-		type = _Rook;
-		break;
-	case IMAGE_WhitePawn:
-		color = _white;
-		type = _Pawn;
-		break;
-	case IMAGE_BlackPawn:
-		color = _black;
-		type = _Pawn;
-		break;
-	}
-	pieces = Index;
-	pieces_Image = BitMapManager::GetInstance()->GetImage(Index);
+	type = _type;
+	color = _color;
 	P_x = x;
 	P_y = y;
 	pices_rect.left = x;
 	pices_rect.top = y;
 	pices_rect.right = pices_rect.left + 80;
 	pices_rect.bottom = pices_rect.top + 80;
+	pieces_Image = BitMapManager::GetInstance()->GetImage(index);
+
 }
 
 bool _Chess::ColliderCheck(POINT point,int _color) //기물과 마우스 위치 확인
@@ -116,7 +66,7 @@ bool _Chess::ColliderCheck(POINT point,int _color) //기물과 마우스 위치 확인
 
 bool _Chess::CheckPieces(int x, int y, std::vector<_Chess*> pieces)
 {
- 	if (x < 0 || x > 560)
+	if (x < 0 || x > 560)
 	{
 		return false;
 	}
