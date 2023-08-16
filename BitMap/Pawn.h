1eaf800a17ae9d0ecd7563d4771df269e04697dc
tjdgus15 <jungyh3030@gmail.com>
2023-08-16 17:44:41 +0900

    Update Pawn.h

--- BitMap/Pawn.h
+++ BitMap/Pawn.h
@@ -10,49 +10,16 @@ public:
 	{
 		if (y == 80)
 		{
-			SetXY(x, y, IMAGE_WhitePawn,_white);
+			SetXY(x, y, IMAGE_WhitePawn,_white,_Pawn);
 		}
 		else
 		{
-			SetXY(x, y, IMAGE_BlackPawn, _black);
+			SetXY(x, y, IMAGE_BlackPawn, _black, _Pawn);
 		}
 	}
 	virtual std::vector<POINT> SetMove(std::vector<_Chess*>pieces) override
 	{
-		/*std::vector<POINT> moveable;
 		
-		int curY = GetY();
-		int curX = GetX();
-
-		int x1 = curX + 80;
-		int x2 = curX - 80;
-		int y1 = curY + 80;
-		int y2 = curY - 80;
-
-		int forward = GetColor() ? 1 : -1;
-		const int size = firstMove ? 1 : 2;
-		for (int i = 0; size > i; i++)
-		{
-			if (CheckPieces(x2, curY + forward * 80 * (i + 1), pieces))
-			{
-				moveable.push_back({ x2, curY + forward * 80 * (i + 1) });
-				continue;
-			}
-
-			break;
-		}
-
-		if (CheckPieces(curX + 80, curY + forward * 80, pieces))
-		{
-			moveable.push_back({ curX + 80, curY + forward * 80 });
-		}
-		if (CheckPieces(curX - 80, curY + forward * 80, pieces))
-		{
-			moveable.push_back({ curX - 80, curY + forward * 80 });
-		}
-
-		
-		return moveable;*/
 		std::vector<POINT> moveable;
 
 		int curY = GetY();
@@ -64,10 +31,13 @@ public:
 		firstMove = false;
 		for (int i = 0; size > i; i++)
 		{
-			if (CheckPieces(curX, curY + forward * 80 * (i + 1), pieces) == true)
+			if (CheckPieces(curX, curY + forward * 80 * (i + 1), pieces) )
 			{
-				moveable.push_back({ curX, curY + forward * 80 * (i + 1) });
-				continue;
+				if (!Checkenemy()) // 상대피스가 있어도 폰은 대각선으로밖에 못움직인다.
+				{
+					moveable.push_back({ curX, curY + forward * 80 * (i + 1) });
+					continue;
+				}
 			}
 
 			break;
