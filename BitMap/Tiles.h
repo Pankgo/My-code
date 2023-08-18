7731aa709e77f06e97704dfef347601aec3cf4c1
tjdgus15 <jungyh3030@gmail.com>
2023-08-18 17:15:48 +0900

    Update Tiles.h

--- BitMap/Tiles.h
+++ BitMap/Tiles.h
@@ -17,18 +17,23 @@ enum tiles_state
 class Tiles
 {
 	int t_x, t_y;
+
 	BitMap* tiles_Image;
 	BitMap* alphabitmap;
+
 	tile tile;
 	RECT _tile;
+
 	tiles_state _tileInfo;
 
 public:
-	void Draw(HDC hdc, CHECKIMAGE check, bool moveabletiles); // 그리는 함수
 	void Init(IMAGE Index,int x,int y);
+	void Draw(HDC hdc, CHECKIMAGE check, bool moveabletiles); // 그리는 함수
+
 	void SetMoveableTiles();
-	int GetTx() { return t_x; }
 	bool ColliderCheck(POINT point); //기물과 마우스 위치 확인
+
+	int GetTx() { return t_x; }
 	int GetTy() { return t_y; }
 	tiles_state Getstate() { return _tileInfo; }
 };
